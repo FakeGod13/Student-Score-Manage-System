4bdba3d70aa18b4baef198f14197bffc098f6ed6
summeriver <83163060+FakeGod13@users.noreply.github.com>
2022-12-09 00:55:03 +0800

    4

--- src/ScoreList/ScoreList.java
+++ src/ScoreList/ScoreList.java
@@ -68,13 +68,62 @@ public class ScoreList {
             if (inputID.equals(list[i].getStudentID()) &&
                     inputCourseName.equals(list[i].getCourseName())) {
                 for (int j = 0; i + j < length - 1; j++) {
-                    list[i+j] = list[i+j+1];
+                    list[i + j] = list[i + j + 1];
                 }
                 System.out.println("删除成功！");
                 return;
             }
         }
         System.out.println("查无此人，删除失败！");
+    }
+
+    public void getCourseScore() {
+        System.out.print("请输入需要查询的课程：");
+        String inputCourseName = input.next();
+        double max = 0;
+        double min = 1000;
+        double sum = 0;
+        int cnt = 0;
+        boolean flag = true;
+        for (int i = 0; i < length; i++) {
+            if (inputCourseName.equals(list[i].getCourseName())) {
+                flag = false;
+                double num = list[i].getScore();
+                max = max > num ? max : num;
+                min = min < num ? min : num;
+                sum = sum + num;
+                cnt = cnt + 1;
+            }
+        }
+        if (flag) {
+            System.out.println("查无此课程，查询失败！");
+            return;
+        }
+        System.out.printf("%-10s", "最高分");
+        System.out.printf("%-10.1d", max);
+        System.out.printf("%-10s", "最低分");
+        System.out.printf("%-10.1d", min);
+        System.out.printf("%-10s", "平均分");
+        System.out.printf("%-10.2d", sum / cnt);
+        System.out.print("\n");
+    }
+
+    public void searchStudentScoreByStudentID() {
+        System.out.print("请输入需要查询的学生学号：");
+        String inputStudentID = input.next();
+        boolean flag = true;
+        for (int i = 0; i < length; i++) {
+            if(inputStudentID.equals(list[i].getStudentID())){
+                flag = false;
+                System.out.printf("%-10s", list[i].getCourseName());
+                System.out.printf("%-10.1d", list[i].getScore());
+                System.out.print("\n");
+            }
+        }
+        if(flag){
+            System.out.println("查无此人，查询失败！");
+            return;
+        }
     }
 
     public void sort() {
